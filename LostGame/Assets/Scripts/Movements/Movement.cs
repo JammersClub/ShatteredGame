b2b0dc72d35c09c59b0d21b7d434e34231254751
Hooman <Hooman.jalalpoor@gmail.com>
2021-01-28 20:44:28 +0330

    player movement fix

--- LostGame/Assets/Scripts/Movements/Movement.cs
+++ LostGame/Assets/Scripts/Movements/Movement.cs
@@ -4,7 +4,7 @@
 
 namespace Movements
 {
-    public class Movement : MonoBehaviour
+    public abstract class Movement : MonoBehaviour
     {
         [SerializeField] private float movementSpeed = 1;
         [SerializeField] private float rotationSpeed = 1;
@@ -15,10 +15,37 @@ namespace Movements
 
         protected float RotationSpeed => rotationSpeed;
 
-        private void Update()
+        private void FixedUpdate()
         {
             transform.position = Vector2.Lerp(transform.position, TargetPosition, Time.deltaTime * movementSpeed);
             transform.rotation = Quaternion.Lerp(transform.rotation, TargetRotation, Time.deltaTime * rotationSpeed);
+        }
+    }
+
+    [RequireComponent(typeof(Rigidbody2D))]
+    public abstract class PhysicalMovement : MonoBehaviour
+    {
+        [SerializeField] private float movementSpeed = 1;
+        [SerializeField] private float rotationSpeed = 1;
+        private Rigidbody2D _rigidbody2D;
+        protected Vector2 TargetPosition;
+        protected Quaternion TargetRotation;
+
+        protected float MovementSpeed => movementSpeed;
+
+        protected float RotationSpeed => rotationSpeed;
+
+        private void Awake()
+        {
+            _rigidbody2D = GetComponent<Rigidbody2D>();
+        }
+
+        private void FixedUpdate()
+        {
+            _rigidbody2D.MovePosition(Vector2.Lerp(_rigidbody2D.position, TargetPosition,
+                Time.deltaTime * movementSpeed));
+            _rigidbody2D.MoveRotation(Quaternion.Lerp(transform.rotation, TargetRotation,
+                Time.deltaTime * rotationSpeed));
         }
     }
 }
--- LostGame/Assets/Scripts/Movements/PlayerMovement.cs
+++ LostGame/Assets/Scripts/Movements/PlayerMovement.cs
@@ -2,23 +2,17 @@
 
 namespace Movements
 {
-    public class PlayerMovement : Movement
+    public class PlayerMovement : PhysicalMovement
     {
-        private Camera _camera;
-
-        private void Start()
-        {
-            _camera = Camera.main;
-        }
-
         // Update is called once per frame
-        private void FixedUpdate()
+        private void Update()
         {
             TargetPosition.y += Input.GetAxis("Vertical") * Time.deltaTime * MovementSpeed;
             TargetPosition.x += Input.GetAxis("Horizontal") * Time.deltaTime * MovementSpeed;
-            var dir = Input.mousePosition - _camera.WorldToScreenPoint(transform.position);
+            var dir = (Vector3) TargetPosition - transform.position;
             var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-            TargetRotation = Quaternion.AngleAxis(angle, Vector3.forward);;
+            TargetRotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            ;
         }
     }
 }
