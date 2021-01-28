88acb4d8ff5bf307b493697e3f7afa1f92c74c77
Hooman <Hooman.jalalpoor@gmail.com>
2021-01-28 13:32:55 +0330

    player wasd movement

---
+++ LostGame/Assets/Scripts/Movements/Movement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+// ReSharper disable Unity.InefficientPropertyAccess
+
+namespace Movements
+{
+    public class Movement : MonoBehaviour
+    {
+        [SerializeField] private float movementSpeed = 1;
+        [SerializeField] private float rotationSpeed = 1;
+        protected Vector2 TargetPosition;
+        protected Quaternion TargetRotation;
+
+        protected float MovementSpeed => movementSpeed;
+
+        protected float RotationSpeed => rotationSpeed;
+
+        private void Update()
+        {
+            transform.position = Vector2.Lerp(transform.position, TargetPosition, Time.deltaTime * movementSpeed);
+            transform.rotation = Quaternion.Lerp(transform.rotation, TargetRotation, Time.deltaTime * rotationSpeed);
+        }
+    }
+}
---
+++ LostGame/Assets/Scripts/Movements/PlayerMovement.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Movements
+{
+    public class PlayerMovement : Movement
+    {
+        // Update is called once per frame
+        private void FixedUpdate()
+        {
+            TargetPosition.y += Input.GetAxis("Vertical") * Time.deltaTime * MovementSpeed;
+            TargetPosition.x += Input.GetAxis("Horizontal") * Time.deltaTime * MovementSpeed;
+        }
+    }
+}
