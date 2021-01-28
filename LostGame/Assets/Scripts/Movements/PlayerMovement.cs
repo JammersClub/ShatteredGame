b555fa4b497ef8f7b1e1a2b46e544676c0bc0573
Hooman <Hooman.jalalpoor@gmail.com>
2021-01-28 13:55:33 +0330

    look at mouse

--- LostGame/Assets/Scripts/Movements/PlayerMovement.cs
+++ LostGame/Assets/Scripts/Movements/PlayerMovement.cs
@@ -4,11 +4,21 @@ namespace Movements
 {
     public class PlayerMovement : Movement
     {
+        private Camera _camera;
+
+        private void Start()
+        {
+            _camera = Camera.main;
+        }
+
         // Update is called once per frame
         private void FixedUpdate()
         {
             TargetPosition.y += Input.GetAxis("Vertical") * Time.deltaTime * MovementSpeed;
             TargetPosition.x += Input.GetAxis("Horizontal") * Time.deltaTime * MovementSpeed;
+            var dir = Input.mousePosition - _camera.WorldToScreenPoint(transform.position);
+            var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            TargetRotation = Quaternion.AngleAxis(angle, Vector3.forward);;
         }
     }
 }
