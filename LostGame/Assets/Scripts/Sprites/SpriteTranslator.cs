59cb47887d44a7a8019558c48c7a4463ef26d5e4
Hooman <Hooman.jalalpoor@gmail.com>
2021-01-29 22:09:08 +0330

    sprite translator fix

--- LostGame/Assets/Scripts/Sprites/SpriteTranslator.cs
+++ LostGame/Assets/Scripts/Sprites/SpriteTranslator.cs
@@ -10,61 +10,35 @@ using UnityEngine;
 namespace Sprites
 {
     [RequireComponent(typeof(SpriteRenderer))]
+    [RequireComponent(typeof(Animator))]
     public class SpriteTranslator : MonoBehaviour
     {
         private const int Offset=62;
-        private const float IdleToWalk=0.2f;
         [SerializeField] private Movement targetMovement;
-        [SerializeField] private CharacterDirectionalSprites idleSpritesList;
-        [SerializeField] private CharacterDirectionalSprites moveSpritesList;
         [SerializeField] private float spriteLerpSpeed=1f;
         private SpriteRenderer _spriteRenderer;
         private float _lastAngle;
         private Transform _cameraTransform;
+        private Animator _animator;
+        private static readonly int Vertical = Animator.StringToHash("Vertical");
+        private static readonly int Horizontal = Animator.StringToHash("Horizontal");
 
         private void Awake()
         {
             // ReSharper disable once PossibleNullReferenceException
             _cameraTransform = Camera.main.transform;
             _spriteRenderer = GetComponent<SpriteRenderer>();
+            _animator = GetComponent<Animator>();
         }
 
         private void Update()
         {
             var position = targetMovement.transform.position;
             transform.position = position;
-            transform.rotation=Quaternion.LookRotation(-_cameraTransform.forward,Vector3.forward);
-            _lastAngle = Mathf.Lerp(_lastAngle, targetMovement.transform.eulerAngles.z, Time.deltaTime*spriteLerpSpeed);
-            _spriteRenderer.sprite = targetMovement.DistanceToTarget < IdleToWalk ? idleSpritesList[_lastAngle + Offset] : moveSpritesList[_lastAngle + Offset];
-        }
-    }
-    [Serializable]
-    public struct CharacterDirectionalSprites
-    {
-        [SerializeField] private Sprite topLeft;
-        [SerializeField] private Sprite topMiddle;
-        [SerializeField] private Sprite topRight;
-        [SerializeField] private Sprite middleLeft;
-        [SerializeField] private Sprite middleRight;
-        [SerializeField] private Sprite downLeft;
-        [SerializeField] private Sprite downMiddle;
-        [SerializeField] private Sprite downRight;
-        private const int Tolerance=10;
-        public Sprite this[float angleToUp]
-        {
-            get
-            {
-                if (Math.Abs(angleToUp-135f)<Tolerance) return topLeft;
-                if (Math.Abs(angleToUp-90f)<Tolerance) return topMiddle;
-                if (Math.Abs(angleToUp-45f)<Tolerance) return topRight;
-                if (Math.Abs(angleToUp-180f)<Tolerance) return middleLeft;
-                if (Math.Abs(angleToUp-225f)<Tolerance) return downLeft;
-                if (Math.Abs(angleToUp-270f)<Tolerance) return downMiddle;
-                if (Math.Abs(angleToUp-315f)<Tolerance) return downRight;
-                if (Math.Abs(angleToUp-360f)<Tolerance) return middleRight;
-                if (Math.Abs(angleToUp-0f)<Tolerance) return middleRight;
-                return downMiddle;
-            }
+            transform.rotation=Quaternion.LookRotation(_cameraTransform.forward,-Vector3.forward);
+            var movement = targetMovement.transform.InverseTransformDirection(-_cameraTransform.forward);
+            _animator.SetFloat(Vertical,movement.y);
+            _animator.SetFloat(Horizontal,movement.x);
         }
     }
 }
