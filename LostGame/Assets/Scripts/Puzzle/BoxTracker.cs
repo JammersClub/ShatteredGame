9eb98339ab846f078053965b063d0abdb9081a7f
Hooman <Hooman.jalalpoor@gmail.com>
2021-01-30 23:31:27 +0330

    box tracker offset

--- LostGame/Assets/Scripts/Puzzle/BoxTracker.cs
+++ LostGame/Assets/Scripts/Puzzle/BoxTracker.cs
@@ -7,11 +7,12 @@ namespace Puzzle
         [SerializeField] private float radiusToDetectBox = 1f;
         [SerializeField] private DoorAuthoring targetDoor;
         [SerializeField] private BoxAuthoring box;
+        [SerializeField] private Vector3 offset;
 
         private void Update()
         {
             //box is in place
-            targetDoor.IsOpen = Vector3.Distance(box.transform.position, transform.position) < radiusToDetectBox;
+            targetDoor.IsOpen = Vector3.Distance(box.transform.position, transform.position+offset) < radiusToDetectBox;
         }
         
     }
