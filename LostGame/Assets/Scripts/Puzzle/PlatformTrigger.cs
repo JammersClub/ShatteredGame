3c75667073bbcbadfd16072e67fce1034b112016
Hooman <Hooman.jalalpoor@gmail.com>
2021-01-30 20:22:04 +0330

    platform trigger

---
+++ LostGame/Assets/Scripts/Puzzle/PlatformTrigger.cs
@@ -0,0 +1,28 @@
+﻿using Movements;
+using Player;
+using UnityEngine;
+
+namespace Puzzle
+{
+    [RequireComponent(typeof(BoxCollider))]
+    [RequireComponent(typeof(DoorAuthoring))]
+    public class PlatformTrigger:MonoBehaviour
+    {
+        private BoxCollider _collider;
+        private DoorAuthoring _doorMode;
+        private Movement _player;
+
+        private void Awake()
+        {
+            _doorMode = GetComponent<DoorAuthoring>();
+            _collider = GetComponent<BoxCollider>();
+            _collider.isTrigger = true;
+            _player = FindObjectOfType<PlayerMovement>();
+        }
+
+        private void Update()
+        {
+            _doorMode.IsOpen = _collider.bounds.Contains(_player.transform.position);
+        }
+    }
+}
