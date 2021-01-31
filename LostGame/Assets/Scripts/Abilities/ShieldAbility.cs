ed2f6e6fdcf4263a3b0574733e2f15488e85c97f
Hooman <Hooman.jalalpoor@gmail.com>
2021-01-31 12:50:02 +0330

    some sound effects

--- LostGame/Assets/Scripts/Abilities/ShieldAbility.cs
+++ LostGame/Assets/Scripts/Abilities/ShieldAbility.cs
@@ -7,8 +7,10 @@ namespace Abilities
     public sealed class ShieldAbility : Ability
     {
         [Tooltip("In Seconds.")] [SerializeField] private float shieldTime=5;
+        [SerializeField] private AudioClip shieldAudioClip;
         private ShieldAuthoring _player;
         private LaserReceiverData _playerLr;
+        private AudioSource _playerAudio;
         private float _timer;
 
         private void Awake()
@@ -18,15 +20,21 @@ namespace Abilities
             {
                 _player = gm.GetComponent<ShieldAuthoring>();
                 _playerLr = gm.GetComponent<LaserReceiverData>();
-                if(_player && _playerLr) enabled = true;
+                _playerAudio = gm.GetComponent<AudioSource>();
+                if(_player && _playerLr && _playerAudio) enabled = true;
             };
         }
 
         private void Update()
         {
             _timer -= Time.deltaTime;
-            if (_timer < 0) _timer = shieldTime * 2;
-            _playerLr.receiveLaser = _timer > shieldTime;
+            if (_timer < 0)
+            {
+                _timer = shieldTime * 2;
+                _playerAudio.PlayOneShot(shieldAudioClip);
+            }
+
+            _playerLr.receiveLaser = _timer < shieldTime;
             _player.ShieldEnabled=!_playerLr.receiveLaser;
         }
     }
