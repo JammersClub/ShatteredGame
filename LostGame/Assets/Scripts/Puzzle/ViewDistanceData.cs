4703622f73ae90464119aac4b028b3f100890bd0
Hooman <Hooman.jalalpoor@gmail.com>
2021-01-31 22:55:05 +0330

    new distance calc

--- LostGame/Assets/Scripts/Puzzle/ViewDistanceData.cs
+++ LostGame/Assets/Scripts/Puzzle/ViewDistanceData.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 namespace Puzzle
 {
     [RequireComponent(typeof(DoorAuthoring))]
-    public class ViewDistanceData:GatherBehaviour<ViewDistanceData>
+    public class ViewDistanceData:GatherBehaviour<ViewDistanceData>,ICorrectPosition
     {
         private DoorAuthoring _doorMode;
+        private bool _hasMeshRenderer;
+        private MeshRenderer _renderer;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            _renderer = GetComponent<MeshRenderer>();
+            _hasMeshRenderer = _renderer;
+        }
 
         private void Start()
         {
             Show = false;
         }
 
+        public Vector3 Center => _hasMeshRenderer ? _renderer.bounds.center : transform.position;
         public bool Show
         {
             get
