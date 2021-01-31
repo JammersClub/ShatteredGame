c4498666af1e4c28ea14188122472f7cb3b4d402
Hooman <Hooman.jalalpoor@gmail.com>
2021-02-01 01:31:07 +0330

    easter sound player code

---
+++ LostGame/Assets/Scripts/Puzzle/PlaySoundByEnteringArea.cs
@@ -0,0 +1,27 @@
+﻿using Movements;
+using Player;
+using UnityEngine;
+
+namespace Puzzle
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class PlaySoundByEnteringArea:MonoBehaviour
+    {
+        private Movement _player;
+        private AudioSource _source;
+        [SerializeField] private float distanceToGm=5f;
+
+        private void Awake()
+        {
+            _player = FindObjectOfType<PlayerMovement>();
+            _source = GetComponent<AudioSource>();
+        }
+
+        private void Update()
+        {
+            if (Vector3.Distance(transform.position, _player.TargetPosition) > distanceToGm) return;
+                _source.Play();
+                enabled = false;
+        }
+    }
+}
