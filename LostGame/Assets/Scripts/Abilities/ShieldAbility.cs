0b147cfee2527404d55fcb8f7bb80e8e5c011427
Hooman <Hooman.jalalpoor@gmail.com>
2021-01-31 14:14:25 +0330

    foot step audio

--- LostGame/Assets/Scripts/Abilities/ShieldAbility.cs
+++ LostGame/Assets/Scripts/Abilities/ShieldAbility.cs
@@ -7,10 +7,8 @@ namespace Abilities
     public sealed class ShieldAbility : Ability
     {
         [Tooltip("In Seconds.")] [SerializeField] private float shieldTime=5;
-        [SerializeField] private AudioClip shieldAudioClip;
         private ShieldAuthoring _player;
         private LaserReceiverData _playerLr;
-        private AudioSource _playerAudio;
         private float _timer;
 
         private void Awake()
@@ -20,20 +18,14 @@ namespace Abilities
             {
                 _player = gm.GetComponent<ShieldAuthoring>();
                 _playerLr = gm.GetComponent<LaserReceiverData>();
-                _playerAudio = gm.GetComponent<AudioSource>();
-                if(_player && _playerLr && _playerAudio) enabled = true;
+                if(_player && _playerLr) enabled = true;
             };
         }
 
         private void Update()
         {
             _timer -= Time.deltaTime;
-            if (_timer < 0)
-            {
-                _timer = shieldTime * 2;
-                _playerAudio.PlayOneShot(shieldAudioClip);
-            }
-
+            if (_timer < 0) _timer = shieldTime * 2;
             _playerLr.receiveLaser = _timer < shieldTime;
             _player.ShieldEnabled=!_playerLr.receiveLaser;
         }
---
+++ LostGame/Assets/Scripts/Player/PlayerFootStepAudioController.cs
@@ -0,0 +1,34 @@
+﻿using Movements;
+using UnityEngine;
+
+namespace Player
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class PlayerFootStepAudioController:MonoBehaviour
+    {
+        private AudioSource _audioSource;
+        [SerializeField] private Movement playerMovement;
+        [SerializeField] private PlayerPlatform platform;
+        [SerializeField] private AudioClip waterFootStepClip;
+        private AudioClip _footStepClip;
+
+        private void Awake()
+        {
+            _audioSource = GetComponent<AudioSource>();
+            _footStepClip = _audioSource.clip;
+        }
+
+        private void Update()
+        {
+            if(playerMovement.Velocity!=Vector3.zero && !_audioSource.isPlaying)
+            {
+                if (waterFootStepClip && !platform.CurrentOrLastPlatform)
+                    _audioSource.clip = waterFootStepClip;
+                else _audioSource.clip = _footStepClip;
+                _audioSource.Play();
+            }
+            else if(playerMovement.Velocity==Vector3.zero && _audioSource.isPlaying)
+                _audioSource.Stop();
+        }
+    }
+}
