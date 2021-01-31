aaf8cb71168cf53b1649f58217c2a50ce8f2037e
Hooman <Hooman.jalalpoor@gmail.com>
2021-01-31 20:45:05 +0330

    better canvas

---
+++ LostGame/Assets/Scripts/Menu/Menu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Menu
+{
+    public class Menu:MonoBehaviour
+    {
+        public void RestartGame()
+        {
+            SceneManager.LoadScene($"MainGame");
+        }
+    }
+}
