04b11462b7e2fb0e64f31eea7b580629ed11a6b2
Hooman <Hooman.jalalpoor@gmail.com>
2021-02-01 19:46:32 +0330

    touch movement

--- ShatteredGame/Assets/Scripts/Player/PlayerMovement.cs
+++ ShatteredGame/Assets/Scripts/Player/PlayerMovement.cs
@@ -18,10 +18,28 @@ namespace Player
             _camera = Camera.main.transform;
         }
 
+        private Vector2 _touchBeginPosition;
+
         private void Update()
         {
-            var movement =
-                _camera.TransformDirection(new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")));
+            Vector3 inputs = default;
+            if (Input.touchCount == 1)
+            {
+                var touch1 = Input.GetTouch(0);
+                // ReSharper disable once SwitchStatementMissingSomeEnumCasesNoDefault
+                switch (touch1.phase)
+                {
+                    case TouchPhase.Began:
+                        _touchBeginPosition = touch1.position;
+                        break;
+                    case TouchPhase.Moved:
+                    case TouchPhase.Stationary:
+                        inputs = Vector3.ClampMagnitude(touch1.position - _touchBeginPosition, 1);;
+                        break;
+                }
+            }
+            else inputs = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            var movement = _camera.TransformDirection(inputs);
             movement.y = 0;
             var velocity = movement.normalized * MovementSpeed;
             velocity.y = Rigidbody.velocity.y;
